220f02f0954d8551e2b339328b0b4832c1f70089
Yoruyeh <orchardsky31@gmail.com>
2023-09-21 09:46:15 +0800

    feat: add browser router and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="/guide"></Route>
+        <Route path="/rate"></Route>
+        <Route path="/stations"></Route>
+        <Route path="/news"></Route>
+        <Route path="/events"></Route>
+        <Route path="*"></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
