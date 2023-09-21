48913041dd5839a964c9b1ff68e023509e698848
Yoruyeh <orchardsky31@gmail.com>
2023-09-21 10:10:33 +0800

    feat: add helpers: navbar items

---
+++ src/utils/helpers.js
@@ -0,0 +1,27 @@
+export const navbarItems = [
+  {
+    id: '1',
+    title: '使用說明',
+    link: 'guide'
+  },
+  {
+    id: '2',
+    title: '收費方式',
+    link: 'rate'
+  },
+  {
+    id: '3',
+    title: '站點資訊',
+    link: 'station'
+  },
+  {
+    id: '4',
+    title: '最新消息',
+    link: 'news'
+  },
+  {
+    id: '5',
+    title: '活動專區',
+    link: 'events'
+  }
+]
