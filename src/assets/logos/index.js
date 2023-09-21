60dd9d2dbad0c0791cb385ef89792dd5e1decb51
Yoruyeh <orchardsky31@gmail.com>
2023-09-21 10:14:40 +0800

    feat: import svg icons

---
+++ src/assets/logos/index.js
@@ -0,0 +1,8 @@
+import { ReactComponent as Logo } from './logo.svg'
+import { ReactComponent as Menu } from './menu_24px.svg'
+import { ReactComponent as Close } from './close_24px.svg'
+import { ReactComponent as Search } from './search_24px.svg'
+import { ReactComponent as Check } from './check_small.svg'
+import { ReactComponent as ArrowDown } from './arrow_dropdown.svg'
+
+export { Logo, Menu, Close, Search, Check, ArrowDown }
