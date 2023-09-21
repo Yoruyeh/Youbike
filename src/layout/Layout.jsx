aabd7f54d5cc0ca3980ef680a104045454dae224
Yoruyeh <orchardsky31@gmail.com>
2023-09-21 12:32:49 +0800

    style: write open menu function and menu styles

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,16 +1,23 @@
 import { NavLink, Outlet } from 'react-router-dom'
 import styles from './layout.module.scss'
-import { Logo, Menu } from '../assets/icons'
+import { Logo, Menu, Close } from '../assets/icons'
 import { navbarItems } from '../utils/helpers'
+import { useState } from 'react'
 
 const Layout = () => {
+  const [openMenu, setOpenMenu] = useState(false)
+
   return (
     <div className={styles.app}>
       <header>
         <div className={styles.logo}>
           <Logo />
         </div>
-        <nav className={styles.navbar}>
+        <nav
+          className={
+            openMenu ? `${styles.navbar} ${styles.opened}` : `${styles.navbar}`
+          }
+        >
           <ul className={styles.navList}>
             {navbarItems.map((item) => (
               <NavLink
@@ -21,17 +28,18 @@ const Layout = () => {
                     ? `${styles.navItem} ${styles.active}`
                     : `${styles.navItem}`
                 }
+                onClick={() => setOpenMenu(false)}
               >
                 <li>{item.title}</li>
               </NavLink>
             ))}
           </ul>
-          <NavLink to="login">
+          <NavLink to="login" onClick={() => setOpenMenu(false)}>
             <button className={styles.loginbtn}>登入</button>
           </NavLink>
         </nav>
-        <div className={styles.menu}>
-          <Menu />
+        <div className={styles.menu} onClick={() => setOpenMenu(!openMenu)}>
+          {!openMenu ? <Menu /> : <Close />}
         </div>
       </header>
       <hr />
