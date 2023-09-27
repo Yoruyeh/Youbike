0f64ee1743c931eae3b92a2291e50c0bbe80ac09
Yoruyeh <orchardsky31@gmail.com>
2023-09-27 23:06:58 +0800

    style: checkbox style and station page style

--- src/page/Stations.jsx
+++ src/page/Stations.jsx
@@ -6,29 +6,34 @@ import SelectButton from '../components/selectButton/SelectButton'
 import CheckBox from '../components/checkBox/CheckBox'
 import DropDown from '../components/dropDown/DropDown'
 import { useState } from 'react'
+import BikingImg from '../assets/images/image.png'
 
 const Stations = () => {
   const [openDropDown, setOpenDropDown] = useState(false)
 
   return (
     <div className={styles.stations}>
-      <div className={styles.filters}>
-        <h1>{GetPageTitle()}</h1>
-        <div className={styles.inputWrapper}>
-          <SearchInput placeholder={'搜尋站點'} />
-          <SelectButton
-            defaultValue={'台灣大學'}
-            onClick={() => 
-              setOpenDropDown(!openDropDown)}
-          >
-            {openDropDown && <DropDown />}
-          </SelectButton>
+      <div className={styles.container}>
+        <div className={styles.filters}>
+          <h1>{GetPageTitle()}</h1>
+          <div className={styles.inputWrapper}>
+            <SearchInput placeholder={'搜尋站點'} />
+            <SelectButton
+              defaultValue={'台灣大學'}
+              onClick={() => setOpenDropDown(!openDropDown)}
+            >
+              {openDropDown && <DropDown />}
+            </SelectButton>
+          </div>
+          <div className={styles.checkboxWrapper}>
+            <CheckBox name={'全部勾選'} />
+            {TaipeDistricts.map((dist) => (
+              <CheckBox key={dist.id} name={dist.title} />
+            ))}
+          </div>
         </div>
-        <div className={styles.checkboxWrapper}>
-          <CheckBox name={'全部勾選'} />
-          {TaipeDistricts.map((dist) => (
-            <CheckBox key={dist.id} name={dist.title} />
-          ))}
+        <div className={styles.image}>
+          <img src={BikingImg} alt="biking" />
         </div>
       </div>
       <StationsTable />
